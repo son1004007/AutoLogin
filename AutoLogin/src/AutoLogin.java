b343608126042f08e62b13f55df66ee95edc607f
son10 <son10@Kiseok-DeskTop>
2023-02-11 23:34:26 +0900

    dev

---
+++ AutoLogin/src/AutoLogin.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.awt.AWTException;
+import java.awt.Desktop;
+import java.awt.Robot;
+
+public class AutoLogin {
+	public static void main(String [] args) throws AWTException {
+		final int VK_ENTER = 10;
+		
+		try {
+			Desktop.getDesktop().browse(new URI("https://www.projectware.kr/A/In.aspx"));
+			Thread.sleep(5000);
+			
+			Robot robot = new Robot();
+			
+			robot.keyPress(VK_ENTER);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
